4fa8ed4ee700f7ad120508ee4cadf1809646743c
Ossama Harach <oharach@temenos.com>
2019-10-13 20:31:18 +0200

    Error handling

--- src/index.js
+++ src/index.js
@@ -30,6 +30,21 @@ app.use('/hadiths', routes.hadith);
 app.use('/comments', routes.comment);
 app.use('/authors', routes.author);
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+      error: {
+          message: error.message
+      }
+  })
+});
+
 const eraseDatabaseOnSync = true;
 
 connectDb().then(async () => {
