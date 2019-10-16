d78d160c10a834d5ff388c372471ba60f19fdd18
Ossama Harach <oharach@temenos.com>
2019-10-16 22:40:48 +0200

    Name field split

--- src/models/user.js
+++ src/models/user.js
@@ -4,7 +4,12 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const userSchema = new mongoose.Schema({
-  name: {
+  firstname: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastname: {
     type: String,
     required: true,
     trim: true
